ec6ebf5f7c21b294eb14352c3be80dd1959f5fbc
swastik pratik <swastiksingh368@gmail.com>
2024-05-15 14:03:56 +0530

    May 15 Problems

---
+++ E_1_Game_with_Marbles_Easy_Version.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define leadingzero __builtin_clzll
+#define trailingzero __builtin_ctzll
+#define bitsParity __builtin_parity
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+#define int long long
+#define vll vector<int>
+#define vbb vector<bool>
+#define vss vector<string>
+#define vpp vector<pair<int, int>>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+const int MOD = 1000000007;
+
+
+//------------------------------ SOLVE -------------------------------------
+/*
+        --> READ PROPERLY (READ AGAIN)
+        --> THINK > FORMULATE > STRUCTURE CODE > CODE
+        --> Check variables, overflows
+        --> DEBUG
+        --> SUBMIT
+ 
+        * special cases (n=1?)
+        * do something instead of nothing and stay organized
+        * WRITE STUFF DOWN
+        * DON'T GET STUCK ON ONE APPROACH
+ 
+        * try to apply brute force first > 
+        * think greedy > 
+        * Don't Overthink about complexity >
+        * Use pen-copy > 
+*/
+
+void super(int test)
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+    vector<pair<int, int>> sm(n); 
+    for(auto &x : a)
+        cin >> x;
+    for(auto &x : b)
+        cin >> x;
+
+    for (int i = 0; i < n;i++){
+        sm[i] = {a[i] + b[i], i};
+    }
+
+    sort(sm.rbegin(), sm.rend());
+
+    int f = 1;
+
+    for (int i = 0;i <n;i++){
+        if(i%2 == 0){
+            a[sm[i].second] -= 1;
+            b[sm[i].second] = 0;
+        }
+        else{
+            a[sm[i].second] = 0;
+            b[sm[i].second] -= 1;
+        }
+    }
+
+    int A = 0, B = 0;
+    for (int i = 0; i < n;i++){
+        A += a[i];
+        B += b[i];
+    }
+    cout << (A - B) << endl;
+}
+
+//------------------------- MAIN -------------------------------------
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int testcases = 1;
+    cin >> testcases;
+
+    int test = 1;
+    while (testcases--)
+    {
+        super(test++);
+    }
+
+    return 0;
+}
