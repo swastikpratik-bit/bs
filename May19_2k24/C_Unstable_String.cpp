66c06f3c065f09696f5fefcd2f959dfcc2f976d5
swastik pratik <swastiksingh368@gmail.com>
2024-05-19 21:24:25 +0530

    May19 Problems

---
+++ May19_2k24/C_Unstable_String.cpp
@@ -0,0 +1,99 @@
+// Problem Link -> https://codeforces.com/problemset/problem/1535/C
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define leadingzero __builtin_clzll
+#define trailingzero __builtin_ctzll
+#define bitsParity __builtin_parity
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+#define int long long
+#define vll vector<int>
+#define vbb vector<bool>
+#define vss vector<string>
+#define vpp vector<pair<int, int>>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+const int MOD = 1000000007;
+
+
+//------------------------------ SOLVE -------------------------------------
+/*
+        --> READ PROPERLY (READ AGAIN)
+        --> THINK > FORMULATE > STRUCTURE CODE > CODE
+        --> Check variables, overflows
+        --> DEBUG
+        --> SUBMIT
+ 
+        * special cases (n=1?)
+        * do something instead of nothing and stay organized
+        * WRITE STUFF DOWN
+        * DON'T GET STUCK ON ONE APPROACH
+ 
+        * try to apply brute force first > 
+        * think greedy > 
+        * Don't Overthink about complexity >
+        * Use pen-copy > 
+*/
+
+void super(int test)
+{
+    string s;
+    cin >> s;
+
+    int ans = 0;
+    int c0 = 0, c1 = 0;
+    for (int i = 0; i < s.size();i++){
+        if(s[i] == '0'){
+            c0++;
+            c1 = 0;
+            ans += c0;
+        }
+        else if (s[i] == '1'){
+            c1++;
+            c0 = 0;
+            ans += c1;
+        }
+        else{
+            c0++;
+            c1++;
+            ans += max(c1, c0);
+        }
+        swap(c1, c0);
+    }
+
+    cout << ans << endl;
+}
+
+//------------------------- MAIN -------------------------------------
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int testcases = 1;
+    cin >> testcases;
+
+    int test = 1;
+    while (testcases--)
+    {
+        super(test++);
+    }
+
+    return 0;
+}
+
